8ef37f5cbdabc5bbeb39bbce3588fc80ec0e11dd
syrshax <selvix728@gmail.com>
2026-05-04 18:46:09 +0200

    ch2 variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("the value of inner scope x is {x}");
+    }
+
+    let x: bool = true;
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
